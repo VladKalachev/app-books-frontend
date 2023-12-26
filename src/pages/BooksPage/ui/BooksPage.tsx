41cccae892166b64eeae514b1c305577a4bb89d6
Corvax <vlvete1990@yandex.ru>
2023-12-26 20:38:26 +0300

    fix

--- src/pages/BooksPage/ui/BooksPage.tsx
+++ src/pages/BooksPage/ui/BooksPage.tsx
@@ -1,35 +1,40 @@
-import { BooksService, IBook } from '@/entities/Book';
-import { Page } from '@/widgets/Page';
-import { useEffect, useState } from 'react';
+import { BooksService, IBook } from "@/entities/Book";
+import { Page } from "@/widgets/Page";
+import { useEffect, useState } from "react";
 
 const BooksPage = () => {
-    const [books, setBooks] = useState<IBook[]>([]);
+  const [books, setBooks] = useState<IBook[]>([]);
 
-    const getBooks = async () => {
-      try {
-        const bookList = await BooksService.fetchUBooks();   
-        setBooks(bookList.data);
-      } catch (error: any) {
-        console.log(error);
-      }
+  const getBooks = async () => {
+    try {
+      const bookList = await BooksService.fetchUBooks();
+      setBooks(bookList.data);
+    } catch (error: any) {
+      console.log(error);
     }
-  
-    useEffect(() => {
-     getBooks();
-      () => {
-        setBooks([]);
-      }
-    }, [])
-  
-    
-    return (
-        <Page data-testid="BooksPage">BooksPage
-            <div>
-                {books?.map((book) => (<div key={book?.id}>{book.title}</div>))}
-            </div>
-    
-        </Page>
-    )
+  };
+
+  useEffect(() => {
+    getBooks();
+    () => {
+      setBooks([]);
+    };
+  }, []);
+
+  return (
+    <Page data-testid="BooksPage">
+      BooksPage
+      <div>sidebar</div>
+      <div>header</div>
+      <div>
+        body
+        {books?.map((book) => (
+          <div key={book?.id}>{book.title}</div>
+        ))}
+      </div>
+      <div>footer</div>
+    </Page>
+  );
 };
 
 export default BooksPage;
