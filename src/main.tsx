5d141421293b4d8bea51f2ff3827fc98cddc9e11
Corvax <vlvete1990@yandex.ru>
2024-05-04 21:57:27 +0300

    chore: add StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App.tsx";
 import { BrowserRouter } from "react-router-dom";
@@ -8,13 +8,13 @@ import "@/app/styles/index.scss";
 import { ThemeProvider } from "./app/providers/ThemeProvider/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <StoreProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </StoreProvider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <StoreProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </StoreProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
